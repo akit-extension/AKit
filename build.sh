2f9fec19de22dbc8f74ca10fe8eacdcad181fe1a
akit-extension <yinenjwv@anappthat.com>
2015-04-14 17:03:37 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,15 +9,6 @@ if [ $1 == "chrome" ] || [ $1 == "all" ]; then
   cp *.js build/chrome
   cp *.css build/chrome
   built=true
-fi
-if [ $1 == "safari" ] || [ $1 == "all" ]; then 
-  echo "Building XKit for Safari"
-  rm -rf build/XKit.safariextension
-  mkdir -p build/XKit.safariextension
-  cp Safari/* build/XKit.safariextension/
-  cp *.js build/XKit.safariextension
-  cp *.css build/XKit.safariextension
-  built=true
 fi 
 if [ $built == false ]; then
   echo "'$1' is not a valid build target."
